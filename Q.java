78ba902196fe5bb9988d667eea8323893c315885
Anish Mohanty <108749478+anish-mohanty@users.noreply.github.com>
2026-01-13 15:35:58 +0530

    Create Q.java

---
+++ Q.java
@@ -0,0 +1,19 @@
+package com.cognizant.banking.models;
+
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = false)
+public class Individual extends Customer {
+
+    private Gender gender;
+    private Date dob;
+
+}
